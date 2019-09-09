719c046427b82bb7135096b9d66c1250660182bc
tclinken <trevorclinkenbeard@gmail.com>
2019-09-09 11:25:43 -0700

    Added --force option to fdbunlocker

--- fdbunlocker/fdbunlocker.actor.cpp
+++ fdbunlocker/fdbunlocker.actor.cpp
@@ -16,15 +16,41 @@
 #include "flow/actorcompiler.h" // This must be the last #include.
 
 void printUsage(const char* name) {
-	printf("usage: %s -C CONNFILE -L LOCKUID\n", name);
+	printf("usage: %s --connfile <CONNFILE> (--lockuid <LOCKUID> | --force)\n", name);
+}
+
+Database getDB(std::string clusterFileName) {
+	std::pair<std::string, bool> resolvedClusterFile = ClusterConnectionFile::lookupClusterFileName(clusterFileName);
+	Reference<ClusterConnectionFile> ccf(new ClusterConnectionFile(resolvedClusterFile.first));
+	return Database::createDatabase(ccf, -1, false);
+}
+
+ACTOR Future<Void> forceUnlockDB(std::string clusterFileName) {
+	try {
+		// does not retry if transaction fails
+		Database db = getDB(clusterFileName);
+		state Transaction tr(db);
+		tr.setOption(FDBTransactionOptions::ACCESS_SYSTEM_KEYS);
+		tr.setOption(FDBTransactionOptions::LOCK_AWARE);
+		Optional<Value> val = wait(tr.get(databaseLockedKey));
+		if (val.present()) {
+			auto lockUID = BinaryReader::fromStringRef<UID>(val.get().substr(10), Unversioned());
+			wait(timeoutError(unlockDatabase(&tr, lockUID), 5.0));
+			wait(tr.commit());
+		}
+		printf("Database unlocked.\n");
+		g_network->stop();
+		return Void();
+	} catch (Error& e) {
+		printf("Error occurred while attempting to unlock database: %s (%d).\n", e.what(), e.code());
+		g_network->stop();
+		throw e;
+	}
 }
 
 ACTOR Future<Void> unlockDB(std::string clusterFileName, UID lockUID) {
 	try {
-		state std::pair<std::string, bool> resolvedClusterFile =
-		    ClusterConnectionFile::lookupClusterFileName(clusterFileName);
-		state Reference<ClusterConnectionFile> ccf(new ClusterConnectionFile(resolvedClusterFile.first));
-		state Database db = Database::createDatabase(ccf, -1, false);
+		Database db = getDB(clusterFileName);
 		wait(timeoutError(unlockDatabase(db, lockUID), 5.0));
 		printf("Databse unlocked.\n");
 		g_network->stop();
@@ -37,18 +63,27 @@ ACTOR Future<Void> unlockDB(std::string clusterFileName, UID lockUID) {
 }
 
 int main(int argc, char** argv) {
-	std::string clusterFileName;
-	UID lockUID;
+	Optional<std::string> clusterFileName;
+	Optional<UID> lockUID;
+	bool force = false;
 
 	for (int i = 1; i < argc; ++i) {
 		std::string arg(argv[i]);
 		if (arg == "-C" || arg == "--connfile") {
+			if (clusterFileName.present()) {
+				printUsage(argv[0]);
+				return 1;
+			}
 			if (i + 1 >= argc) {
 				printf("Expecting an argument after %s\n", argv[i]);
 				return 1;
 			}
 			clusterFileName = std::string(argv[++i]);
 		} else if (arg == "-L" || arg == "--lockuid") {
+			if (lockUID.present() || force) {
+				printUsage(argv[0]);
+				return 1;
+			}
 			if (i + 1 >= argc) {
 				printf("Expecting an argument after %s\n", argv[i]);
 				return 1;
@@ -59,11 +94,17 @@ int main(int argc, char** argv) {
 				return 1;
 			}
 			lockUID = UID::fromString(lockUIDString);
+		} else if (arg == "-f" || arg == "--force") {
+			if (lockUID.present() || force) {
+				printUsage(argv[0]);
+				return 1;
+			}
+			force = true;
 		} else {
 			printf("Unexpected argument: %s\n", argv[i]);
 		}
 	}
-	if (!lockUID.isValid() || clusterFileName.empty()) {
+	if (!(force ^ lockUID.present()) || !clusterFileName.present()) {
 		printUsage(argv[0]);
 		return 1;
 	}
@@ -73,7 +114,7 @@ int main(int argc, char** argv) {
 		initSignalSafeUnwind();
 		registerCrashHandler();
 		setupNetwork();
-		auto f = unlockDB(clusterFileName, lockUID);
+		auto f = force ? forceUnlockDB(clusterFileName.get()) : unlockDB(clusterFileName.get(), lockUID.get());
 		runNetwork();
 	} catch (Error& e) {
 		printf("ERROR: %s (%d)\n", e.what(), e.code());
