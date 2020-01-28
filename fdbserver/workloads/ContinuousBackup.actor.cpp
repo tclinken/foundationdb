9a2efc7273343058b458e60da78b8b60c3669763
tclinken <trevorclinkenbeard@gmail.com>
2020-01-27 17:21:41 -0800

    Added ContinuousBackup workload

---
+++ fdbserver/workloads/ContinuousBackup.actor.cpp
@@ -0,0 +1,76 @@
+/*
+ * ContinuosBackup.actor.cpp
+ *
+ * This source file is part of the FoundationDB open source project
+ *
+ * Copyright 2013-2018 Apple Inc. and the FoundationDB project authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "fdbrpc/simulator.h"
+#include "fdbclient/BackupAgent.actor.h"
+#include "fdbclient/BackupContainer.h"
+#include "fdbserver/workloads/workloads.actor.h"
+#include "flow/actorcompiler.h" // This must be the last #include.
+
+struct ContinuousBackupWorkload : TestWorkload {
+
+	Standalone<StringRef> backupDir;
+	Standalone<StringRef> tag;
+	FileBackupAgent backupAgent;
+	bool submitOnly;
+	bool abortOnly;
+
+	ContinuousBackupWorkload(WorkloadContext const& wcx) : TestWorkload(wcx) {
+		backupDir = getOption(options, LiteralStringRef("backupDir"), LiteralStringRef("simfdb/backup/"));
+		tag = getOption(options, LiteralStringRef("tag"), LiteralStringRef("default"));
+		submitOnly = getOption(options, LiteralStringRef("submitOnly"), false);
+		abortOnly = getOption(options, LiteralStringRef("abortOnly"), false);
+	}
+
+	virtual std::string description() { return "ContinuousBackup"; }
+
+	virtual Future<Void> setup(Database const& cx) { return Void(); }
+
+	virtual Future<Void> start(Database const& cx) {
+		if (clientId || abortOnly) {
+			return Void();
+		}
+		return _start(cx, this);
+	}
+
+	virtual Future<bool> check(Database const& cx) {
+		if (clientId != 0 || submitOnly)
+			return true;
+		else
+			return _check(cx, this);
+	}
+
+	ACTOR static Future<Void> _start(Database cx, ContinuousBackupWorkload* self) {
+		Standalone<VectorRef<KeyRangeRef>> backupRanges;
+		backupRanges.push_back_deep(backupRanges.arena(), normalKeys);
+		wait(self->backupAgent.submitBackup(cx, self->backupDir.withPrefix(LiteralStringRef("file://")), 1e8,
+		                                    self->tag.toString(), backupRanges, false));
+		return Void();
+	}
+
+	ACTOR static Future<bool> _check(Database cx, ContinuousBackupWorkload* self) {
+		wait(self->backupAgent.abortBackup(cx, self->tag.toString()));
+		return true;
+	}
+
+	virtual void getMetrics(vector<PerfMetric>& m) {}
+};
+
+WorkloadFactory<ContinuousBackupWorkload> ContinuousBackupWorkloadFactory("ContinuousBackup");
